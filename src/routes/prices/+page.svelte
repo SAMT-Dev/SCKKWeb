33a58332d0259c23503640bf7dad93e5c13d8f6f
Csanó <hvcsano924@gmail.com>
2025-07-03 14:56:35 +0200

    updated prices

--- src/routes/prices/+page.svelte
+++ src/routes/prices/+page.svelte
@@ -18,31 +18,15 @@
 	<h1 class="text-taxi font-nunito text-5xl font-bold xl:text-3xl">Áraink</h1>
 	<div class="flex gap-8">
 		<div class="flex flex-col items-center gap-2">
-			<h2 class="font-poppins text-3xl font-light text-cyan-400">Hétköznap</h2>
 			<div>
-				<h2 class="font-bold">15:00 - 18:30</h2>
-				<h2 class="font-light">2600 $/km, 350 $/perc</h2>
-			</div>
-			<div>
-				<h2 class="font-bold">18:30 - 22:00</h2>
-
+				<h2 class="font-bold">15:00 - 22:00</h2>
 				<h2 class="font-light">2500 $/km, 350 $/perc</h2>
 			</div>
 			<div>
 				<h2 class="font-bold">Idősávokon kívül</h2>
-				<h2 class="font-light">3800 $/km, 400 $/perc</h2>
+				<h2 class="font-light">4000 $/km, 400 $/perc</h2>
 			</div>
 		</div>
-		<div class="flex flex-col items-center gap-2">
-			<h2 class="font-poppins text-3xl font-light text-cyan-400">Hétvégén</h2>
-			<div>
-				<h2 class="font-bold">15:00 - 22:00</h2>
-				<h2 class="font-light">2600 $/km, 250 $/perc</h2>
-			</div>
-			<div>
-				<h2 class="font-bold">Idősávokon kívül</h2>
-				<h2 class="font-light">3500 $/km, 300 $/perc</h2>
-			</div>
-		</div>
+		<h1 class="text-gray-400">Utoljára módosítva: 2025.07.03.</h1>
 	</div>
 </div>
